88ae433feede9b05356f3ea8b82679b58a0f17b9
Carlo Mendoza <carlodgmendoza@gmail.com>
2022-06-28 11:20:17 +0800

    Add unit tests for App

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,22 @@
 import { shallow } from "enzyme";
 import App from "./App";
+import NavBar from "./components/general/NavBar";
+import Footer from "./components/general/Footer";
 
 describe("<App/>", () => {
   let wrapper;
 
   beforeEach(() => (wrapper = shallow(<App />)));
 
-  it("should render a div", () => {
+  it("should render the App div", () => {
     expect(wrapper.find("div.App").length).toEqual(1);
+  });
+
+  it("should render the NavBar", () => {
+    expect(wrapper.containsMatchingElement(<NavBar />)).toEqual(true);
+  });
+
+  it("should render the Footer", () => {
+    expect(wrapper.containsMatchingElement(<Footer />)).toEqual(true);
   });
 });
